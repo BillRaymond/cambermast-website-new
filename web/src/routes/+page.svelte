578369b371e24824a9a6cbed8c3b2ccb48734fca
Bill Raymond <bill.raymond@cambermast.com>
2025-10-02 19:23:09 +0000

    add favicon, apple touch, and android

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -18,16 +18,67 @@
 	<link rel="icon" href="/images/cambermast-favicon.ico" />
 
 	<!-- Apple Touch Icons -->
+	<link rel="apple-touch-icon" href="/images/cambermast-apple-touch-icon.png" />
 	<link rel="apple-touch-icon" sizes="57x57" href="/images/cambermast-apple-touch-icon-57x57.png" />
 	<link rel="apple-touch-icon" sizes="60x60" href="/images/cambermast-apple-touch-icon-60x60.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="/images/cambermast-apple-touch-icon-72x72.png" />
 	<link rel="apple-touch-icon" sizes="76x76" href="/images/cambermast-apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="/images/cambermast-apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="/images/cambermast-apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="/images/cambermast-apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="/images/cambermast-apple-touch-icon-152x152.png" />
-	<link rel="apple-touch-icon" sizes="167x167" href="/images/cambermast-apple-touch-icon-167x167.png" />
-	<link rel="apple-touch-icon" sizes="180x180" href="/images/cambermast-apple-touch-icon-180x180.png" />
+	<link
+		rel="apple-touch-icon"
+		sizes="114x114"
+		href="/images/cambermast-apple-touch-icon-114x114.png"
+	/>
+	<link
+		rel="apple-touch-icon"
+		sizes="120x120"
+		href="/images/cambermast-apple-touch-icon-120x120.png"
+	/>
+	<link
+		rel="apple-touch-icon"
+		sizes="144x144"
+		href="/images/cambermast-apple-touch-icon-144x144.png"
+	/>
+	<link
+		rel="apple-touch-icon"
+		sizes="152x152"
+		href="/images/cambermast-apple-touch-icon-152x152.png"
+	/>
+	<link
+		rel="apple-touch-icon"
+		sizes="167x167"
+		href="/images/cambermast-apple-touch-icon-167x167.png"
+	/>
+	<link
+		rel="apple-touch-icon"
+		sizes="180x180"
+		href="/images/cambermast-apple-touch-icon-180x180.png"
+	/>
+
+	<!-- Android Icons -->
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="192x192"
+		href="/images/cambermast-android-chrome-192x192.png"
+	/>
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="256x256"
+		href="/images/cambermast-android-chrome-256x256.png"
+	/>
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="384x384"
+		href="/images/cambermast-android-chrome-384x384.png"
+	/>
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="512x512"
+		href="/images/cambermast-android-chrome-512x512.png"
+	/>
 </svelte:head>
 
 <!-- Full-bleed hero -->
