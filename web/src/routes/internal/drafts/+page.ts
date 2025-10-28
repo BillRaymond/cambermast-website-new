cb7f2ae14d11811be9d34729e8cc13369c85dfa5
Bill Raymond <bill.raymond@cambermast.com>
2025-10-28 17:52:56 +0000

    add /internal/drafts route to see draft programs and sessions

---
+++ web/src/routes/internal/drafts/+page.ts
@@ -0,0 +1,13 @@
+import { listExternalEvents } from '$lib/data/external-events';
+import { listTrainingPrograms } from '$lib/data/training';
+
+import type { PageLoad } from './$types';
+
+export const prerender = true;
+
+export const load: PageLoad = () => {
+	return {
+		trainingPrograms: listTrainingPrograms({ includeDrafts: true }),
+		externalEvents: listExternalEvents()
+	};
+};
